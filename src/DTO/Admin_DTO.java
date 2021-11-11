a4b87b8fa3b231022008adb97fbc7ccae3ebdd86
bxq9510 <90017872+bxq9510@users.noreply.github.com>
2021-11-11 17:29:12 +0900

    baek's DAO&DTO init

---
+++ src/DTO/Admin_DTO.java
@@ -0,0 +1,20 @@
+package DTO;
+
+public class Admin_DTO {
+	private String adm_id = "admin";
+	private String adm_pw = "1111";
+	
+	public String getAdm_id() {
+		return adm_id;
+	}
+	public void setAdm_id(String adm_id) {
+		this.adm_id = adm_id;
+	}
+	public String getAdm_pw() {
+		return adm_pw;
+	}
+	public void setAdm_pw(String adm_pw) {
+		this.adm_pw = adm_pw;
+	}
+	
+}
